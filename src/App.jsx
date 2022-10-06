993457fa29e227899dbb9a1bc5a48ad1da269ead
caicodes <caicodes1@gmail.com>
2022-10-06 12:09:11 -0600

    updated keys

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
 import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
-import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
- 
+import {
+  ArtistDetails,
+  TopArtists,
+  AroundYou,
+  Discover,
+  Search,
+  SongDetails,
+  TopCharts,
+} from './pages';
+
 const App = () => {
   const { activeSong } = useSelector((state) => state.player);
 
