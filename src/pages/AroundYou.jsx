0fa9e6642192981484a09eb38002b40c3d15c030
caicodes <caicodes1@gmail.com>
2022-10-11 21:31:30 -0600

    adjust geo ipify feature adjusted queries

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,31 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useSelector } from 'react-redux'
 
-import { Error, Loader, SongCard } from '../components';
-import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+import { Error, Loader, SongCard } from '../components'
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore'
 
 const CountryTracks = () => {
-  const [country, setCountry] = useState('');
-  const [loading, setLoading] = useState(true);
-  const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+  const [country, setCountry] = useState('')
+  const [loading, setLoading] = useState(true)
+  const { activeSong, isPlaying } = useSelector((state) => state.player)
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country)
 
   useEffect(() => {
     axios
-      .get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
+      .get(
+        `https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY
+        }`,
+      )
       .then((res) => setCountry(res?.data?.location.country))
       .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
-  }, [country]);
+      .finally(() => setLoading(false))
+  }, [country])
 
-  if (isFetching && loading) return <Loader title="Loading Songs around you..." />;
+  if (isFetching && loading) {
+    return <Loader title="Loading Songs around you..." />
+  }
 
-  if (error && country !== '') return <Error />;
+  if (error && country !== '') return <Error />
 
   return (
     <div className="flex flex-col">
-      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around you <span className="font-black">{country}</span></h2>
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Around you <span className="font-black">{country}</span>
+      </h2>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
@@ -40,7 +47,7 @@ const CountryTracks = () => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CountryTracks;
+export default CountryTracks
